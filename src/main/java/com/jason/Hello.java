98e7b09b9a7d70d9c354b3f18449f2666721c79e
YanKai <asd0989020308@gmail.com>
2019-01-28 13:25:37 +0800

    first commit

---
+++ src/main/java/com/jason/Hello.java
@@ -0,0 +1,7 @@
+package com.jason;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/kotlin/com/jason/Hello.kt
@@ -0,0 +1,13 @@
+package com.jason
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin");
+    val h = Human();
+    h.hello();
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin");
+    }
+}
